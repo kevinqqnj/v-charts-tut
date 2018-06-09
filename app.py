490aced0f3b229ba6a8a0e2a84f54efb14a40273
kevinqqnj <ezhqing@gmail.com>
2018-06-09 11:19:02 +0800

    d3

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
+    return '<h1>my Tutorial of V-charts and D3</h1><br><br>' \
+           '<a href="/vc">v-charts</a><br>' \
+           '<a href="/d3/a">D3 - a</a>'
+
+@app.route('/vc')
+def vcharts():
     return render_template('vc.html')
 
 @app.route('/d3/<string:chapter>')
