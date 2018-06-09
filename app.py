3b5f708665e663ca90aa32a242837a33b5614f37
kevinqqnj <ezhqing@gmail.com>
2018-06-09 10:56:13 +0800

    d3

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('vc.html')
 
+@app.route('/d3/<string:chapter>')
+def d3(chapter):
+    return render_template(f'd3-{chapter}.html')
+
 
 if __name__ == '__main__':
     app.run()
